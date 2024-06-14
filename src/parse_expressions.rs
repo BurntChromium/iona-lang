262a62b75dfaca422e88187f02a20bf04f6cd760
Burnt Chrome <BurntChromium@outlook.com>
2024-06-14 00:03:51 -0400

    Draft of expression parser

--- src/parse_expressions.rs
+++ src/parse_expressions.rs
@@ -1,11 +1,18 @@
 //! # Expression Parsing
 //!
-//! This is a (large) submodule of the parser dedicated to parsing expressions (such as `sqrt 37`). It is an implementation of a Pratt Parser (or a Top Down Operator Precedence Parser).
+//! This is a (large) submodule of the parser dedicated to parsing expressions (such as `sqrt 37`).
+//!
+//! For now we only support prefix operators (like lisp)
+//!
+//! It will probably eventually be an implementation of a Pratt Parser (or a Top Down Operator Precedence Parser).
 //!
 //! All named functions are prefix operations. Some basic mathematical operations (and potentially overloads?) are infix operations.
 
+use std::collections::BTreeMap;
+
 use crate::compiler_errors::{CompilerProblem, ProblemClass};
-use crate::lex::Token;
+use crate::lex::{Symbol, Token};
+use crate::parse::FunctionData;
 
 pub enum Operator {
     Add,
@@ -29,18 +36,34 @@ impl Operator {
             _ => 10,
         }
     }
+
+    fn from_symbol(symbol: Symbol) -> Option<Operator> {
+        match symbol {
+            Symbol::OpPlus => Some(Operator::Add),
+            Symbol::OpMinus => Some(Operator::Subtract),
+            Symbol::OpMul => Some(Operator::Multiply),
+            Symbol::OpDiv => Some(Operator::Divide),
+            _ => None,
+        }
+    }
 }
 
 pub enum Expression {
-    Prefix {
-        op: Operator,
-        args: Vec<Object>,
-    },
-    Infix {
-        left: Box<Object>,
-        op: Operator,
-        right: Box<Object>,
-    },
+    Prefix { op: Operator, args: Vec<Object> },
+    // Infix {
+    //     left: Box<Object>,
+    //     op: Operator,
+    //     right: Option<Box<Object>>,
+    // },
+}
+
+impl Expression {
+    pub fn get_bp(&self) -> u8 {
+        match &self {
+            Expression::Prefix { op, .. } => op.binding_power(),
+            // Expression::Infix { op, .. } => op.binding_power()
+        }
+    }
 }
 
 pub enum Literal {
@@ -100,20 +123,142 @@ pub enum Object {
     Value(Literal),
 }
 
-pub fn parse_expression(tokens: &Vec<Token>) -> Result<Object, CompilerProblem> {
-    // let mut lbp: u8 = 0;
-    for _token in tokens.iter() {
-        // Check if it's a function
+pub fn push_fn_to_stack(
+    token: &Token,
+    op: Operator,
+    arg_count: usize,
+    stack: &mut Vec<Object>,
+) -> Option<CompilerProblem> {
+    let mut args: Vec<Object> = Vec::with_capacity(arg_count);
+    // Do we have enough objects on the stack to satisfy the fn call?
+    if stack.len() < arg_count {
+        return Some(CompilerProblem::new(
+            ProblemClass::Error,
+            &format!("not enough arguments when calling function {}", &token.text),
+            "partial functions are not yet supported by the compiler",
+            token.line,
+            token.word,
+        ));
+    }
+    // Pop the last N objects off the stack and move them into the function's arguments (N == fn.args.len)
+    args.extend(stack.drain(stack.len() - arg_count..));
+    // Finally, push this fn onto the stack
+    stack.push(Object::Operation(Expression::Prefix { op: op, args: args }));
+    None
+}
 
-        // If not, it must be a value
-        match Literal::from_str(&tokens[0].text) {
-            Ok(lit) => return Ok(Object::Value(lit)),
-            Err(mut e) => {
-                e.line = tokens[0].line;
-                e.word_index = tokens[0].word;
-                return Err(e);
+/// Currently only supports prefix operations
+pub fn parse_expression(
+    tokens: &Vec<Token>,
+    fn_table: &BTreeMap<String, FunctionData>,
+) -> Result<Object, CompilerProblem> {
+    // Sanity check
+    if tokens.is_empty() {
+        return Err(CompilerProblem::new(
+            ProblemClass::Error,
+            "expression has no tokens",
+            "make sure to provide a value or call a function here",
+            0,
+            0,
+        ));
+    }
+    // We will push and pop objects/expressions onto a stack
+    let mut stack: Vec<Object> = Vec::with_capacity(tokens.len());
+    // Iterate backwards over the tokens
+    for token in tokens.iter().rev() {
+        match token.symbol {
+            Symbol::OpPlus => {
+                let outcome = push_fn_to_stack(token, Operator::Add, 2, &mut stack);
+                if let Some(e) = outcome {
+                    return Err(e);
+                }
+            }
+            Symbol::OpMinus => {
+                let outcome = push_fn_to_stack(token, Operator::Subtract, 2, &mut stack);
+                if let Some(e) = outcome {
+                    return Err(e);
+                }
+            }
+            Symbol::OpMul => {
+                let outcome = push_fn_to_stack(token, Operator::Multiply, 2, &mut stack);
+                if let Some(e) = outcome {
+                    return Err(e);
+                }
+            }
+            Symbol::OpDiv => {
+                let outcome = push_fn_to_stack(token, Operator::Divide, 2, &mut stack);
+                if let Some(e) = outcome {
+                    return Err(e);
+                }
+            }
+            Symbol::Value => {
+                // Check if it's a function
+                if fn_table.contains_key(&token.text) {
+                    // Check how many arguments it takes
+                    let arg_count = fn_table.get(&token.text).unwrap().args.len();
+                    let outcome = push_fn_to_stack(
+                        token,
+                        Operator::Function {
+                            name: token.text.clone(),
+                        },
+                        arg_count,
+                        &mut stack,
+                    );
+                    if let Some(e) = outcome {
+                        return Err(e);
+                    }
+                } else {
+                    // If not, it must be a value
+                    match Literal::from_str(&token.text) {
+                        Ok(lit) => {
+                            stack.push(Object::Value(lit));
+                        }
+                        // If it's not a value, throw an error
+                        Err(mut e) => {
+                            e.line = token.line;
+                            e.word_index = token.word;
+                            return Err(e);
+                        }
+                    }
+                }
+            }
+            _ => {
+                return Err(CompilerProblem::new(
+                    ProblemClass::Error,
+                    "unimplemented symbol found in expression",
+                    "please wait for compiler update",
+                    token.line,
+                    token.word,
+                ))
             }
         }
     }
-    Ok(Object::Value(Literal::Bool(true)))
+    if stack.is_empty() {
+        let line_no: usize;
+        let word: usize;
+        if let Some(t) = tokens.get(0) {
+            line_no = t.line;
+            word = t.word;
+        } else {
+            line_no = 0;
+            word = 0;
+        }
+        return Err(CompilerProblem::new(
+            ProblemClass::Error,
+            "empty expression",
+            "make sure to provide a value or call a function here",
+            line_no,
+            word,
+        ));
+    } else if stack.len() == 1 {
+        return Ok(stack.pop().unwrap());
+    } else {
+        return Err(CompilerProblem::new(
+            ProblemClass::Error,
+            "too many objects left on the expression stack after parsing",
+            "you probably have passed too many arguments to a function",
+            tokens.last().unwrap().line,
+            tokens.last().unwrap().word,
+        ));
+    }
 }
